ce3e54f7272486ce7226984c2e667ecb3e430213
fatihsimsek <fatih.simsek.cm@gmail.com>
2016-10-17 14:03:50 +0300

    init

--- AKSwiftSlideMenu/HomeViewController.swift
+++ AKSwiftSlideMenu/HomeViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class HomeViewController: BaseViewController {
 
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var surname: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func showName(_ sender: UIButton) {
+        var fullname : String = ""
+        
+        if let txtName = name.text {
+            fullname += txtName
+        }
+        
+        if let txtSurname = surname.text {
+            fullname += (" " + txtSurname)
+        }
+    
+        let alert = UIAlertController(title: "HoşGeldin", message: fullname, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
