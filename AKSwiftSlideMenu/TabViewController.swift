e5333aea7bcc1d2271fb477ddb9d5267cd0e3d6a
fatihsimsek <fatih.simsek.cm@gmail.com>
2016-10-20 14:54:04 +0300

    initial

--- AKSwiftSlideMenu/TabViewController.swift
+++ AKSwiftSlideMenu/TabViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class TabViewController: BaseViewController {
+class TabViewController: BaseViewController, UITabBarDelegate {
 
+    @IBOutlet weak var tabControl: UITabBar!
+    @IBOutlet weak var vwRect: UIView!
+    @IBOutlet weak var lblTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
-        // Do any additional setup after loading the view.
+        
+        self.tabControl.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if(item.tag == 0) {
+            self.lblTitle.text = "Red"
+            self.vwRect.backgroundColor = UIColor.red
+        }
+        else if(item.tag == 1) {
+            self.lblTitle.text = "Blue"
+            self.vwRect.backgroundColor = UIColor.blue
+        }
+        else {
+            self.lblTitle.text = "Black"
+            self.vwRect.backgroundColor = UIColor.black
+        }
+    }
 }
